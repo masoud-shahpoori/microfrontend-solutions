2b9be888ba7308362a8054e7eab8ce40e06c9558
masoud shahpoori <masoudshahpoori97@gmail.com>
2022-12-10 15:42:53 +0330

    change [app-telegram]: webpack config

--- apps/app-telegram-conf/src/index.tsx
+++ apps/app-telegram-conf/src/index.tsx
@@ -1,9 +1,11 @@
-//@ts-ignore
 import React from 'react';
-//@ts-ignore
 import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+// const AuthenticationAppRemote = React.lazy(() => import("account/App"));
+
+
+
 root.render(
   <React.StrictMode>
     hii
